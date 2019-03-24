cf52833f4a4570cc8caf49d4fd43452acd6adfa8
Chris <cnelson1200@gmail.com>
2019-03-24 11:48:43 -0700

    posts to django app

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from ModbusServer import ModbusServer
 from RegisterRepo import RegisterRepo
 from ModbusValues import *
 
+import requests
+
 rr = RegisterRepo()
 ms = ModbusServer(rr)
 # ih = InputHandler()
@@ -17,14 +19,16 @@ values = [
     ModbusFloat("TempSensor(f)", 85, rr),
     ModbusFloat("Pannel(V)", solarValsStart, rr, fixedPoint=100),
     ModbusFloat("Pannel(A)", solarValsStart + 1 * scale, rr, fixedPoint=100),
-    ModbusFloat("Pannel(PL)", solarValsStart + 2 * scale, rr, fixedPoint=100),
-    ModbusFloat("Pannel(PH)", solarValsStart + 3 * scale, rr, fixedPoint=100),
-    ModbusFloat("Battery(PL)", solarValsStart + 6 * scale, rr, fixedPoint=100),
-    ModbusFloat("Battery(PH)", solarValsStart + 7 * scale, rr, fixedPoint=100),
-    ModbusFloat("Load(C)", solarValsStart + 13 * scale, rr, fixedPoint=100),
-    ModbusFloat("Load(v)", solarValsStart + 12 * scale, rr, fixedPoint=100),
+    ModbusFloat("Pannel(WL)", solarValsStart + 2 * scale, rr, fixedPoint=100),
+    ModbusFloat("Pannel(WH)", solarValsStart + 3 * scale, rr, fixedPoint=100),
+    ModbusFloat("Battery(V)", solarValsStart + 4 * scale, rr, fixedPoint=100),
+    ModbusFloat("Battery(A)", solarValsStart + 5 * scale, rr, fixedPoint=100),
+    ModbusFloat("Battery(WL)", solarValsStart + 6 * scale, rr, fixedPoint=100),
+    ModbusFloat("Battery(WH)", solarValsStart + 7 * scale, rr, fixedPoint=100),
+    ModbusFloat("Load(I)", solarValsStart + 13 * scale, rr, fixedPoint=100),
+    ModbusFloat("Load(V)", solarValsStart + 12 * scale, rr, fixedPoint=100),
 
-    ModbusFloat("TempCase(f)", solarValsStart + 17 * scale, rr, fixedPoint=100 * 5/9, offset=32),
+    ModbusFloat("TempCase(f)", solarValsStart + 17 * scale, rr, fixedPoint=100 * 5 / 9, offset=32),
 
 ]
 
@@ -33,4 +37,16 @@ while threading.active_count() > 0:
     for v in values:
         print(str(v))
     print('\n')
+
     time.sleep(2)
+    r = requests.post("http://127.0.0.1:8000/logTempData/", data={'tempInF': values[0].value, })
+    print(r.status_code, r.reason)
+    r = requests.post("http://127.0.0.1:8000/logSystemStatus/", data=
+    {
+        'panelVoltage': values[1].value,
+        'panelCurrent': values[2].value,
+        'batteryVoltage': values[5].value,
+        'loadVoltage': values[9].value,
+        'loadCurrent': values[10].value,
+    })
+    print(r.status_code, r.reason)
